8cd4daace1131f412930ccad24a52ffc9abe1a2e
Samuel Adeosun <olusanmi.adeosun@gmail.com>
2025-07-01 11:41:59 +0100

    Conflict

---
+++ Source/Core-OS/RAM.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdbool.h>
+typedef struct Header{
+    size_t Size;
+
+}Header;
+typedef struct HeaderContext{
+
+}HeaderContext;
--- Source/OS/Functions/Functions.h
+++
@@ -1,4 +0,0 @@
-#define DefaultHashLevel 3
-
-void Hash_ToEqual(uint8_t *Data, unsigned int HashLevel);
-uint8_t Hash_ToDigit(uint8_t *Data, unsigned int HashLevel);
