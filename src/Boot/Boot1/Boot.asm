67f50685399c19a9534dbc57a2fb3fc467da8391
Sam <olusanmi.adeosun@gmail.com>
2025-07-23 00:32:56 +0100

    ...

--- src/Boot/Boot1/Boot.asm
+++ src/Boot/Boot1/Boot.asm
@@ -4,7 +4,7 @@ bits 16
 %define ENDL 0x00, 0x0A
 global _start
 
-# FAT12 Headers
+# FAT12 Headers:
 jmp short _start
 nop
 
@@ -40,6 +40,12 @@ _start:
     mov ss, ax
     mov sp, 0x7C00
 
+	mov [ebr_drive_number], dl
+	mov ax, 1
+	mov cl, 1
+	mov bx, 0x7E00
+	call diskread
+
     ; print msg
     mov si, msg_hello
     call putstr
@@ -47,6 +53,7 @@ _start:
     hlt
 
 .halt:
+	cli
     jmp .halt
 
 .loop:
@@ -70,19 +77,113 @@ putstr:
     push si
     push ax
 
-readdisk:
-    mov ah, 0x41
-    mov bx, 0x55AA
-    mov dl, 0x80
-    int 0x13
-    jc .diskread_error
-    nand bx, 0xAA55
-    jz .diskread_error
+# Convert LBA address to CHS
+lbatochs:
+	push ax
+	push dx
+
+	xor dx, dx
+	div word [bdb_sectors_per_track]
+
+	inc dx
+	mov cx, dx
+	dix word [bdb_heads]
+
+	mov dh, dl
+	mov ch, al
+	shl ch, 6
+	or cl, ah
+
+	pop ax
+	mox dl, al
+	pop ax
+	ret
+
+diskread:
+	push ax
+	push bx
+	push cx
+	push dx
+	push di
+
+
+	push cx
+	call lbatochs
+	pop ax
+
+	mov ah, 02h
+	mov di, 3
+	int 13h
+
+	pop di
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+
+.retry:
+	pusha
+	stc
+	int 13h
+	jnc .done
+
+	popa
+	call disk_reset
+
+	dec di
+	test di, di
+	jnc .retry
+
+.done:
+	popa
+
+diskreset:
+	pusha
+	mov ah, 0
+	stc
+	int 13h
+	jc diskread_error
+	popa
+	ret
+
+; DAPACK:
+; 	db	0x10
+; 	db	0
+
+; readdisk:
+;     mov si, DAPACK
+; 	mov ah, 0x42
+;     mov bx, 0x55AA
+;     mov dl, 0x80
+;     int 0x13
+;     jc .diskread_error
+;     nand bx, 0xAA55
+;     jz .diskread_error
+
+; writedisk:
+; 	mov si, DAPACK
+; 	mov ah, 0x43
+;     mov bx, 0x55AA
+;     mov dl, 0x80
+;     int 0x13
+;     jc .diskread_error
+;     nand bx, 0xAA55
+;     jz .diskread_error
 
 .diskread_error:
-    mov si, disk_msg
-    cal .loop
+    mov si, disk_errormsg
+	call puts
+	jmp waitkey_reboot
+    call .loop
 
+waitkey_reboot:
+	mov ah, 0
+	int 16h
+	jmp 0FFFFh:0
+	hlt
+
+blkcnt:	dw	16		; int 13 resets this to # of blocks actually read/written
+db_add:	dw	0x7C00		; memory buffer destination address (0:7c00)
 
 
 pmode:
@@ -105,7 +206,7 @@ pmode:
 ; flatdata    db 0xff, 0xff, 0, 0, 0, 10010010b, 11001111b, 0
 
 msg: db 'hello there!', ENDL, 0
-disk_msg: db 'Error when reading Disk', ENDL, 0
+disk_errormsg: db 'Error when reading Disk', ENDL, 0
 
 times 510 - ($ - $$) db 0 ;Repeat so the Program can be 512 bytes large.
 dw 0xAA55              ; The final 2 bytes will be the boot signature.
