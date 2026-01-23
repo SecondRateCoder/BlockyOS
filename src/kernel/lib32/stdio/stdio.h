560cefc407530fff91c2afa63294c5ebc913a31e
Sam <olusanmi.adeosun@gmail.com>
2026-01-23 12:42:31 +0000

    Update stdio.h
    Organisation and Prototype declarations.

--- src/kernel/lib32/stdio/stdio.h
+++ src/kernel/lib32/stdio/stdio.h
@@ -12,12 +12,21 @@ void printf32(char *fmt, ...);
 #define printarg
 char *printarg32(uinl32_t *argp, uint8_t dwords, bool sign, uint8_t radix, bool printin, bool attach_sign);
 
+// Function Prototypes
 void updateCursor32();
 void puts32(char *str);
 void putc32(char c, uint8_t color);
+
+// Wrapper prototypes
+static inline getCursor32(uinl32_t *X, uinl32_t *Y);
 static inline void scrollCursor32(uint8_t lines);
 
+// VGA prototypes
 extern void __attribute__((cdecl)) asm_enableCursor32(void);
 extern void __attribute__((cdecl)) asm_disdableCursor32(void);
 extern uint16_t __attribute__((cdecl)) asm_getCursor32();
 extern void __attribute__((cdecl)) asm_updateCursor32(uinl32_t x, uinl32_t y);
+
+// IO.asm prototypes
+extern void __attribute__((cdecl)) _outb(uint16_t port, uint8_t value);
+extern uint8_t __attribute__((cdecl)) _inb(uint16_t port);
