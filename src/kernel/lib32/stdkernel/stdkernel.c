a527bec8e7090d0faa38172a4337c23438fe380f
Sam <olusanmi.adeosun@gmail.com>
2026-01-27 12:07:09 +0000

    Update stdkernel.c
    ...

--- src/kernel/lib32/stdkernel/stdkernel.c
+++ src/kernel/lib32/stdkernel/stdkernel.c
@@ -2,3 +2,22 @@
 #include "GDT.h"
 
 void __attribute__((cdecl)) isr_handlerC(){return;}
+
+void SetInterrupt(int interrupt, bool present, void *base, uint16_t segDesc, uint8_t flags){
+    if(interrupt > 0 && interrupt < 256){
+        IDT[interrupt] = (IDTentry){
+            .baseLow = (uint32_t)base & 0xFFFF,
+            .segDesc = segDesc,
+            .reserved = 0,
+            .flags = flags | (present? IDTFLAG_PRESENT: 0),
+            .baseLow = ((uint32_t)base >> 16) & 0xFFFF
+        }
+    }
+}
+
+void ToggleInterrupt(uint8_t interrupt, bool present){
+    if(interrupt > 0 && interrupt < 256){
+        if(present){IDT[interrupt].flag |= IDTFLAG_PRESENT;}
+        else{IDT[interrupt].flag &= ~IDTFLAG_PRESENT;}
+    }
+}
