198ae8a152d6bf9584e49a35a7b622953b74c851
Sam <olusanmi.adeosun@gmail.com>
2026-01-27 11:17:14 +0000

    Update Interrupt.h
    Added InterruptFrame and RegisterFrame structs.

--- src/kernel/lib32/stdkernel/Interrupt.h
+++ src/kernel/lib32/stdkernel/Interrupt.h
@@ -2,5 +2,24 @@
 
 #include "./kernel/public/public/math/math.h"
 
+typedef struct InterruptFrame{
+    uint32_t eip;
+    uint16_t cs;
+    uint32_t flags;
+    uint32_t sp,
+    uint16_t ss;
+}__attribute__((packed)) InterruptFrame;
+
+typedef struct RegisterFrame{
+    uint32_t eax,
+             ecx,
+             edx,
+             ebx,
+             esp,
+             ebp,
+             esi,
+             edi;
+}__attribute__((packed)) RegisterFrame;
+
 extern void __attribute__((cdecl)) LoadIDT(void *ptr);
-void __attribute__((cdecl)) isr_handlerC();
+void __attribute__((cdecl)) isr_handlerC(InterruptFrame *IFrame, RegisterFrame *RFrame);
