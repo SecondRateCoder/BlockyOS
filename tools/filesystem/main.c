133ff87dc67179045e7a068b086eae39f170a581
Sam <olusanmi.adeosun@gmail.com>
2026-04-28 14:54:22 +0100

    Update main.c
    Added testing code for Read/Writing File.

--- tools/filesystem/main.c
+++ tools/filesystem/main.c
@@ -9,7 +9,14 @@ void main(uint32_t largs, char **arg){
             printf("Drive Error! Killing Program");
             exit(EXIT_FAILURE);
         }
+        printf("\n\n");
         free(str);
         fshandle *handle = fsloadh(fr, "attempt\0", "fc");
+        void *_block = malloc(getblocksize());
+        for(uint32_t cc = 0; cc < getblocksize(); ++cc){printf("%c:%u    ", ((uint8_t *)_block)[cc], ((uint8_t *)_block)[cc]);}
+        _fpush1(handle, _block);
+        free(_block);
+        _block = _fread1(handle);
+        for(uint32_t cc = 0; cc < getblocksize(); ++cc){printf("%c:%u    ", ((uint8_t *)_block)[cc], ((uint8_t *)_block)[cc]);}
     }
 }
